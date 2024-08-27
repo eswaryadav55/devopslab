fd9244c49b1b889fc54dad44d1a57c8555952a9d
AdmiN <AdmiN@ITWS0046>
2024-08-27 11:02:46 +0530

    commit

---
+++ src/nopackage/Devops.java
@@ -0,0 +1,11 @@
+package nopackage;
+public class Devops {
+	public static void main(String[] args) {
+		String k="Kasi";
+		int p=0;
+		for(int i=0;i<k.length();i++) {
+			p++;
+		}
+		System.out.println("Name is "+k+" Length of word is "+p);
+	}
+}
